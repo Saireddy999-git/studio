2c3a06c833d3f54c6b8bf7110f882a8514688fcd
Sai Reddy <reddybellam143@gmail.com>
2025-08-05 15:24:19 +0000

    change icon with 3 kaslalu

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,11 +1,11 @@
-import { UtensilsCrossed } from 'lucide-react';
+import { Sparkle } from 'lucide-react';
 import Link from 'next/link';
 
 export function Logo() {
   return (
     <Link href="/" className="flex items-center gap-2 group">
-      <div className="p-2 bg-primary text-primary-foreground rounded-full group-hover:bg-primary/90 transition-colors">
-        <UtensilsCrossed className="h-6 w-6" />
+      <div className="p-2 bg-primary text-primary-foreground rounded-full group-hover:bg-primary/90 transition-colors flex gap-px">
+        <Sparkle className="h-5 w-5" />
       </div>
       <span className="text-2xl font-headline font-bold text-primary">
         Mudu Kalalu
